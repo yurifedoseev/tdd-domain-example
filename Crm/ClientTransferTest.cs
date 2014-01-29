ae8f6b4f06fc28192c7594fc4ac8946c9c29a909
Yuri Fedoseev <fedoseev.yuri@gmail.com>
2014-01-30 01:02:48 +0400

    Первые тест: определяем модели

--- Crm/ClientTransferTest.cs
+++ Crm/ClientTransferTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Management.Instrumentation;
+using NUnit.Framework;
 
 namespace Crm
 {
@@ -8,7 +9,24 @@ namespace Crm
         [Test]
         public void should_transfer_client_from_one_manager_to_another()
         {
-            
+            var client = new Client {Name = "ООО Вектор Плюс"};
+            var firstManager = new Manager("Петров");
+            var targetManager = new Manager("Сидоров");
         }
+    }
+    
+    public class Client
+    {
+        public string Name { get; set; }
+    }
+
+    public class Manager
+    {
+        public Manager(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; protected set; }
     }
 }
