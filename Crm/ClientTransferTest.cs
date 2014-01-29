9faa8d9a60771c0155505165509f9cc7c9820cab
Yuri Fedoseev <fedoseev.yuri@gmail.com>
2014-01-30 00:49:51 +0400

    Init

---
+++ Crm/ClientTransferTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Crm
+{
+    [TestFixture]
+    public class ClientTransferTest
+    {
+        [Test]
+        public void should_transfer_client_from_one_manager_to_another()
+        {
+            
+        }
+    }
+}
